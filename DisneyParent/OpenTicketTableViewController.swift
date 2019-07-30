3187bd9ad2c2ee15b83dc5782a23b85b1defb27f
bradleyyin <bradleyyin@gmail.com>
2019-07-30 14:32:47 -0700

    linking view and view controller

---
+++ DisneyParent/OpenTicketTableViewController.swift
@@ -0,0 +1,41 @@
+//
+//  OpenTicketTableViewController.swift
+//  DisneyParent
+//
+//  Created by Bradley Yin on 7/30/19.
+//  Copyright © 2019 bradleyyin. All rights reserved.
+//
+
+import UIKit
+
+class OpenTicketTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let detailVC = segue.destination as? TicketDetailsViewController else {return}
+        if segue.identifier == "OpenTicketDetailShowSegue" {
+            detailVC.fromMyTicket = false
+            print("openticket")
+        }
+    }
+}
